499691895fdeca92fd64f12a9fa599cd1ba4e440
Palchhin <palchhinparihar@gmail.com>
2025-12-17 18:41:02 +0530

    fix: update server port configuration to use environment variable

--- bot.py
+++ bot.py
@@ -18,9 +18,12 @@ app = Flask('')
 def home():
   return "Bot is alive!"
 
-
+# ---------------------------
+# Run server
+# ---------------------------
 def run():
-  app.run(host='0.0.0.0', port=8080)
+  port = int(os.environ.get("PORT", 10000))
+  app.run(host="0.0.0.0", port=port)
 
 
 def keep_alive():
